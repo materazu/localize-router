dd3f0483daf16d5a9399c6c88f07cea8f729b255
Julien Moulin <julien@baliz.org>
2020-02-28 10:51:13 +0100

    Update localize-router.module.ts
    Fix for angular 9

--- src/localize-router.module.ts
+++ src/localize-router.module.ts
@@ -70,7 +70,7 @@ export function getAppInitializer(p: ParserInitializer, parser: LocalizeParser, 
 })
 export class LocalizeRouterModule {
 
-  static forRoot(routes: Routes, config: LocalizeRouterConfig = {}): ModuleWithProviders {
+  static forRoot(routes: Routes, config: LocalizeRouterConfig = {}): ModuleWithProviders<LocalizeRouterModule> {
     return {
       ngModule: LocalizeRouterModule,
       providers: [
